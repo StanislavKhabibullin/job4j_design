c16e410ccc08e726e3c0c211c480516293137be4
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2020-11-02 08:34:52 +0300

    0. Инструменты [#385688]

---
+++ chapter_001/src/main/java/ru/job4j/it/EventIt.java
@@ -0,0 +1,37 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EventIt implements Iterator<Integer> {
+    private final int[] data;
+    private int rsl = 0;
+
+    public EventIt(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        for (int i = rsl; i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        for (int i = rsl; i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                rsl = i;
+                break;
+            }
+        }
+        return data[rsl++];
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/it/MatrixIt.java
@@ -0,0 +1,38 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while ((row < data.length) && column >= data[row].length) {
+            row++;
+            column = 0;
+        }
+        for (int i = row; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if (Integer.valueOf(data[i][j]) != null) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[row][column++];
+    }
+}

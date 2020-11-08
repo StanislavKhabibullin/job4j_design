a0b634c1c30e2327c2c04f6ab582433fa2b0fe36
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2020-11-08 21:18:32 +0300

    0. Что такое обобщенные типы (generics) [#385705]

---
+++ chapter_001/src/test/java/ru/job4j/generics/Animal.java
@@ -0,0 +1,38 @@
+package ru.job4j.generics;
+
+public class Animal {
+    private String kindOfAnimal;
+
+    public Animal() {
+        this.kindOfAnimal = "NoAnimal";
+    }
+
+    public Animal(String kindOfAnimal) {
+        this.kindOfAnimal = kindOfAnimal;
+    }
+
+    public String getKindOfAnimal() {
+        return kindOfAnimal;
+    }
+
+    public void setKindOfAnimal(String kindOfAnimal) {
+        this.kindOfAnimal = kindOfAnimal;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "kindOfAnimal='" + kindOfAnimal + '\'' +
+                '}';
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/generics/Predator.java
@@ -0,0 +1,10 @@
+package ru.job4j.generics;
+
+public class Predator extends Animal {
+    public Predator() {
+    }
+
+    public Predator(String kindOfAnimal) {
+        super(kindOfAnimal);
+    }
+}

a479e480493ef1dce02e6d825f4c10c7c3fda221
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2020-11-19 22:52:06 +0300

    5.2.1. Реализовать SimpleArray<T> [#385703]

--- chapter_001/src/test/java/ru/job4j/generics/Programmer.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.generics;
-
-import java.util.Date;
-
-public class Programmer extends Person {
-    public Programmer(String name, int age, Date data) {
-        super(name, age, data);
-    }
-}
--- chapter_001/src/test/java/ru/job4j/generics/SimpleArrayTest.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.generics;
-
-import junit.framework.TestCase;
-
-public class SimpleArrayTest extends TestCase {
-
-
-}

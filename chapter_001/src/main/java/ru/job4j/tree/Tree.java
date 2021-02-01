e742ae305b358c8f9f5d0ebdbca586c0aea6f74b
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-02-01 07:49:02 +0300

    1. Создать элементарную структуру дерева [#385699]

---
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,45 @@
+package ru.job4j.tree;
+
+import java.util.Optional;
+import java.util.*;
+
+public class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+
+    public Tree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> rsl = findBy(parent);
+
+        if (rsl.isPresent()) {
+            for (Node<E> val:
+                 rsl.get().children) {
+                if (findBy(child).isPresent()) {
+                    return false;
+                }
+            }
+            rsl.get().children.add(new Node<>(child));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}

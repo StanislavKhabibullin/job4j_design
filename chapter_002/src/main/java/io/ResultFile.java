9a7c40d2aed1867daf7024f7bcb8e78256f0a643
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-02-24 21:49:48 +0300

    0.1. FileOutputStream. [#385730]

--- chapter_002/src/main/java/io/ResultFile.java
+++ chapter_002/src/main/java/io/ResultFile.java
@@ -5,9 +5,19 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("Hellow World! /n".getBytes());
-            for (int i = 0; i < 10; i++) {
-                out.write(1);
+            out.write("Hellow World! \n".getBytes());
+            for (int i = 0; i <= 10; i++) {
+                for (int j = 0; j <= 10; j++) {
+
+                    out.write(Integer.toString(i).getBytes());
+                    out.write('*');
+                    out.write(Integer.toString(j).getBytes());
+                    out.write('=');
+                    int result = i * j;
+                    out.write(Integer.toString(result).getBytes());
+                    out.write("   \n".getBytes());
+                }
+                out.write('\n');
             }
         } catch (Exception e) {
             e.printStackTrace();

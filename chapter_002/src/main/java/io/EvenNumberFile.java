3c3b97f14325b9c0bd748515d44d5236ca206c96
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-02-25 08:58:23 +0300

    0.2. FileInputStream [#385740]

---
+++ chapter_002/src/main/java/io/EvenNumberFile.java
@@ -0,0 +1,33 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")){
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String value:
+                 lines) {
+                Integer num = Integer.valueOf(value);
+                if (num % 2 == 0) {
+
+                    System.out.println(num);
+                }
+
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

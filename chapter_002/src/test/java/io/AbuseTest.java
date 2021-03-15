6812bc4bd7e96049bcfefe71e8f9bc2930e77d09
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-03-15 16:30:32 +0300

    3.0. Тестирование IO [#385728]

---
+++ chapter_002/src/test/java/io/AbuseTest.java
@@ -0,0 +1,33 @@
+package io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class AbuseTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void drop() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)){
+            out.print("hello foolish dude");
+        }
+        Abuse.drop(source.getAbsolutePath(), target.getAbsolutePath(), List.of("foolish"));
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))){
+            in.lines().forEach(s -> rsl.append(s));
+        }
+        assertThat(rsl.toString(), is("hello dude "));
+    }
+
+
+}
--- chapter_002/src/test/java/io/AnalizyTest.java
+++ chapter_002/src/test/java/io/AnalizyTest.java
@@ -1,17 +1,38 @@
 package io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void WhenUseTemporaryFolder() throws IOException {
+        File source = folder.newFile("source.csv");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)){
+            out.println("300 10:10:10");
+            out.println("400 10:12:10");
+            out.println("500 10:14:14");
+            out.println("200 10:20:10");
+            out.println("300 11:10:10");
+        }
+        Analizy test = new Analizy();
+        test.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))){
+            reader.lines().forEach(line -> rsl.append(line));
+        }
+        assertThat(rsl.toString(), is("10:12:10;10:20:10"));
+    }
 
     @Test
     public void WhenPairOneSwitchOut() {

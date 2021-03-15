03a86f98dcbb871c60d9cc38943561867531b8b4
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-03-15 11:05:49 +0300

    2. Анализ доступности сервера. [#385737]

--- chapter_002/src/main/java/io/Analizy.java
+++ chapter_002/src/main/java/io/Analizy.java
@@ -1,22 +1,38 @@
 package io;
 
-import javax.swing.text.html.HTMLDocument;
 import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Analizy {
+    Integer scramb = 0;
     public void unavailable(String source, String target) {
-        Map<String, Integer> expect = new LinkedHashMap<>();
+  //      Map<String, Integer> expect = new LinkedHashMap<>();
+        List<String> result = new ArrayList<>();
+
         try (BufferedReader read = new BufferedReader(
                 new FileReader(source)
         )){
-
             read.lines().forEach(s -> {
                 String[] mas = s.split(" ");
                 if (!mas[0].equals("")) {
-                    expect.put(mas[1], Integer.valueOf(mas[0]));
+                    if ((result.size() == 0) && (mas[0].equals("400") || mas[0].equals("500"))) {
+                        result.add(mas[1]);
+                        scramb = Integer.valueOf(mas[0]);
+
+                    }
+                    if ((mas[0].equals("200") || mas[0].equals("300")) && (scramb != 0)) {
+                        result.add(mas[1]);
+                        scramb = 0;
+                    }
+                    if ((mas[0].equals("400") || mas[0].equals("500")) && (scramb != 0)) {
+
+                    }
+                    if ((mas[0].equals("400") || mas[0].equals("500")) && (scramb == 0)) {
+                        result.add(mas[1]);
+                        scramb = Integer.valueOf(mas[0]);
+                    }
+
                 }
             });
 
@@ -28,7 +44,7 @@ public class Analizy {
 
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
 
-            List<String> result = new ArrayList<>();
+         /*   List<String> result = new ArrayList<>();
             Integer scramb = 0;
             for (Map.Entry <String, Integer> entry:
                  expect.entrySet()) {
@@ -52,6 +68,8 @@ public class Analizy {
                     scramb = value;
                 }
             }
+
+          */
 
             for (int i = 0; i < result.size(); i++) {
                 out.print(result.get(i));

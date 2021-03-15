d86af767b86f34e0ecec2db614697cad4e16fb2d
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-03-15 19:20:41 +0300

    4.0. File [#385733]

---
+++ chapter_002/src/main/java/io/Dir.java
@@ -0,0 +1,23 @@
+package io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("file is not exist %s",
+                    file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("file is not directory %s",
+                    file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s",
+                file.getTotalSpace()));
+        for (File subFile:
+             file.listFiles()) {
+            System.out.println("file name - " + subFile.getName() + " file size : " + subFile.length());
+        }
+    }
+}

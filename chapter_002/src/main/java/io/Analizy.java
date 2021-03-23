9beb769814b3ce622f60fb2724b4f18d3afd3175
StanislavKhabibullin <66030700+StanislavKhabibullin@users.noreply.github.com>
2021-03-23 21:43:10 +0300

    2. Анализ доступности сервера. [#385737]

--- chapter_002/src/main/java/io/Analizy.java
+++ chapter_002/src/main/java/io/Analizy.java
@@ -5,27 +5,31 @@ import java.util.*;
 
 
 public class Analizy {
-    Integer scramb = 0;
+
     public void unavailable(String source, String target) {
+        Integer scramb = 0;
         List<String> result = new ArrayList<>();
 
         try (BufferedReader read = new BufferedReader(
                 new FileReader(source)
         )) {
-            read.lines().forEach(s -> {
-                String[] mas = s.split(" ");
-                if (!mas[0].equals("")) {
-                    if ((mas[0].equals("200") || mas[0].equals("300")) && (scramb != 0)) {
-                        result.add(mas[1]);
-                        scramb = 0;
-                    }
-                    if ((mas[0].equals("400") || mas[0].equals("500")) && (scramb == 0)) {
-                        result.add(mas[1]);
-                        scramb = Integer.valueOf(mas[0]);
-                    }
+            var lin = read.readLine();
+            while (lin != null) {
 
-                }
-            });
+                    String[] mas = lin.split(" ");
+                    if (!mas[0].equals("")) {
+                        if ((mas[0].equals("200") || mas[0].equals("300")) && (scramb != 0)) {
+                            result.add(mas[1]);
+                            scramb = 0;
+                        }
+                        if ((mas[0].equals("400") || mas[0].equals("500")) && (scramb == 0)) {
+                            result.add(mas[1]);
+                            scramb = Integer.valueOf(mas[0]);
+                        }
+
+                    }
+                lin = read.readLine();
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();

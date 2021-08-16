59c2e5569497e0cae7b31188d410e28de24b7e92
Stanislav <skhabibullin@yandex.ru>
2021-08-16 13:55:12 +0300

    1. Принципы Kiss, Dry и Yagni [#238813]

--- chapter_005/src/main/java/kiss/MaxMin.java
+++ chapter_005/src/main/java/kiss/MaxMin.java
@@ -7,24 +7,47 @@ import java.util.List;
 
 public class MaxMin {
 
+    public <T> T sravn(List<T> value, Comparator<T> comparator, String maxMin) {
+        T resultMin = value.get(0);
+        T resultMax = value.get(0);
+        for (int i = 1; i < value.size(); i++) {
+            if (comparator.compare(resultMax, value.get(i)) < 0) {
+                resultMax = value.get(i);
+            }
+            if (comparator.compare(resultMin, value.get(i)) > 0) {
+                resultMin = value.get(i);
+            }
+        }
+        if (maxMin.equals("max")) {
+            return resultMax;
+        }
+        return resultMin;
+    }
+
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T maxik = value.get(0);
+       /* T maxik = value.get(0);
         for (int i = 1; i < value.size(); i++) {
            if (comparator.compare(maxik, value.get(i)) <  0) {
                  maxik = value.get(i);
            }
         }
         return maxik;
+
+        */
+        return sravn(value, comparator, "max");
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        T minik = value.get(0);
+        /* T minik = value.get(0);
         for (int i = 1; i < value.size(); i++) {
             if (comparator.compare(minik, value.get(i)) >  0) {
                      minik = value.get(i);
             }
         }
         return minik;
+
+         */
+        return sravn(value, comparator, "min");
     }
 
 }

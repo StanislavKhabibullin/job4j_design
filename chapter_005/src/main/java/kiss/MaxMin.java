3afc3ecf68b83a5502a5ca650a33bd94bbdddac9
Stanislav <skhabibullin@yandex.ru>
2021-08-17 15:23:27 +0300

    1. Принципы Kiss, Dry и Yagni [#238813 #189933]

--- chapter_005/src/main/java/kiss/MaxMin.java
+++ chapter_005/src/main/java/kiss/MaxMin.java
@@ -4,50 +4,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MaxMin {
 
-    public <T> T sravn(List<T> value, Comparator<T> comparator, String maxMin) {
-        T resultMin = value.get(0);
-        T resultMax = value.get(0);
+    public <T> T sravn(List<T> value, Comparator<T> comparator,  Predicate<Integer> predicate) {
+        T result = value.get(0);
         for (int i = 1; i < value.size(); i++) {
-            if (comparator.compare(resultMax, value.get(i)) < 0) {
-                resultMax = value.get(i);
-            }
-            if (comparator.compare(resultMin, value.get(i)) > 0) {
-                resultMin = value.get(i);
+            if (predicate.test(comparator.compare(result, value.get(i)))) {
+                result = value.get(i);
             }
         }
-        if (maxMin.equals("max")) {
-            return resultMax;
-        }
-        return resultMin;
+        return result;
     }
 
     public <T> T max(List<T> value, Comparator<T> comparator) {
-       /* T maxik = value.get(0);
-        for (int i = 1; i < value.size(); i++) {
-           if (comparator.compare(maxik, value.get(i)) <  0) {
-                 maxik = value.get(i);
-           }
-        }
-        return maxik;
-
-        */
-        return sravn(value, comparator, "max");
+        Predicate<Integer> predicateMax = p -> p < 0;
+        return sravn(value, comparator, predicateMax);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        /* T minik = value.get(0);
-        for (int i = 1; i < value.size(); i++) {
-            if (comparator.compare(minik, value.get(i)) >  0) {
-                     minik = value.get(i);
-            }
-        }
-        return minik;
-
-         */
-        return sravn(value, comparator, "min");
+        Predicate<Integer> predicateMin = p -> p > 0;
+        return sravn(value, comparator, predicateMin);
     }
 
 }

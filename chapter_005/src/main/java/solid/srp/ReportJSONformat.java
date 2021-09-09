f45c90679c189eb09f0d1244881779a0c53685e4
Stanislav <skhabibullin@yandex.ru>
2021-09-09 21:03:56 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851]

--- chapter_005/src/main/java/solid/srp/ReportJSONformat.java
+++ chapter_005/src/main/java/solid/srp/ReportJSONformat.java
@@ -20,15 +20,15 @@ public class ReportJSONformat implements Report {
     }
 
     public String serializeToJSON(List<Employee> list) {
-        String rest;
+        //String rest;
         StringBuilder text = new StringBuilder();
         Gson gson = new GsonBuilder().create();
         try {
-            File templeFile = Files.createTempFile("dataForJSON", null).toFile();
+            File templeFile = new  File("./bookJSON.txt");
             xtemp = templeFile;
             try (FileOutputStream fos = new FileOutputStream(templeFile);
                  ObjectOutputStream ois = new ObjectOutputStream(fos)) {
-                for (Employee employee
+               /* for (Employee employee
                         : list) {
                     text.append(gson.toJson(employee))
                             .append(System.lineSeparator());
@@ -36,6 +36,11 @@ public class ReportJSONformat implements Report {
                     rest = gson.toJson(employee);
                     ois.writeObject(rest);
                 }
+
+                */
+                var lib = gson.toJson(list);
+                ois.writeObject(lib);
+
             }
         } catch (EOFException exc) {
 
@@ -44,8 +49,8 @@ public class ReportJSONformat implements Report {
         }
         return String.valueOf(text);
     }
-
-    private List<Employee> deSerializeFromJSON() {
+/*
+    public List<Employee> deSerializeFromJSON() {
         List<Employee> resultAfterSeri = new ArrayList<>();
         try (FileInputStream fis = new FileInputStream(xtemp);
              ObjectInputStream oisJ = new ObjectInputStream(fis)) {
@@ -65,12 +70,16 @@ public class ReportJSONformat implements Report {
         return resultAfterSeri;
     }
 
+ */
+
+
+
 
     @Override
     public String generate(Predicate<Employee> filter) {
         List<Employee> result = store.findBy(filter);
         String text = serializeToJSON(result);
-        deSerializeFromJSON();
+       // deSerializeFromJSON();
         return text;
     }
 
@@ -79,9 +88,12 @@ public class ReportJSONformat implements Report {
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 100);
         Employee worker2 = new Employee("Ivan", now, now, 105);
+        Employee worker3 = new Employee("Ivanna", now, now, 501);
         store.add(worker);
         store.add(worker2);
+        store.add(worker3);
         Report engine = new ReportJSONformat(store);
         System.out.println(engine.generate(tr -> true));
+
     }
 }

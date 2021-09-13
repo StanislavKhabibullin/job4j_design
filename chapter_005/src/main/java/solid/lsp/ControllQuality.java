2df8e6927830e9432860cdff5e564a59568cf7f4
Stanislav <skhabibullin@yandex.ru>
2021-09-13 19:17:58 +0300

    1. Хранилище продуктов [#852 #202050]

--- chapter_005/src/main/java/solid/lsp/ControllQuality.java
+++ chapter_005/src/main/java/solid/lsp/ControllQuality.java
@@ -4,36 +4,43 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ControllQuality {
 
-   Store warehouse = new Warehouse();
-   Store shop = new Shop();
-   Store trash = new Trash();
+   List<Store> storeList;
 
 
+    public ControllQuality(List<Store> storeList) {
+        this.storeList = storeList;
+    }
 
     public Food handler(Food goods) {
-        if (warehouse.accept(goods)) {
-            warehouse.addStore(goods);
-        }
-        if (shop.accept(goods)) {
-            shop.addStore(goods);
-        }
-        if (trash.accept(goods)) {
-            trash.addStore(goods);
+
+        for (Store storrr:
+             storeList) {
+           if (storrr.accept(goods)) {
+               storrr.addStore(goods);
+           }
         }
         return goods;
     }
 
     public void printer() {
-        warehouse.print();
-        shop.print();
-        trash.print();
+        for (Store storrr:
+                storeList) {
+            storrr.print();
+        }
     }
 
     public static void main(String[] args) {
-
+        Store warehouse = new Warehouse();
+        Store shop = new Shop();
+        Store trash = new Trash();
+        List<Store> storeList1 = new ArrayList<>();
+        storeList1.add(warehouse);
+        storeList1.add(shop);
+        storeList1.add(trash);
         System.out.println(LocalDate.now());
         Bread blackBread = new Bread("Bojole", LocalDate.of(2021, 10, 22),
                 LocalDate.of(2021, 9, 10), 100, 0 );
@@ -41,7 +48,7 @@ public class ControllQuality {
                 LocalDate.of(2021, 9, 10), 150, 0 );
         Bread bunBread = new Bread("Bun", LocalDate.of(2021, 9, 14),
                 LocalDate.of(2021, 9, 10), 120, 0 );
-        ControllQuality rez = new ControllQuality();
+        ControllQuality rez = new ControllQuality(storeList1);
         rez.handler(blackBread);
         rez.handler(whiteBread);
         rez.handler(bunBread);
